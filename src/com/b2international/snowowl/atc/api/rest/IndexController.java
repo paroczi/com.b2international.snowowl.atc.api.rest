97d0b14a7907dd2b1add82ee7e92c04537be4933
Paróczi Gergő <paroczi@gmail.com>
2017-11-02 23:25:03 +0100

    get consepts

--- src/com/b2international/snowowl/atc/api/rest/IndexController.java
+++ src/com/b2international/snowowl/atc/api/rest/IndexController.java
@@ -3,10 +3,46 @@ package com.b2international.snowowl.atc.api.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.b2international.snowowl.atc.core.AtcCoreActivator;
+import com.b2international.snowowl.atc.core.domain.AtcConcepts;
+import com.b2international.snowowl.atc.core.request.AtcRequests;
+import com.b2international.snowowl.core.ApplicationContext;
+import com.b2international.snowowl.core.api.IBranchPath;
+import com.b2international.snowowl.core.exceptions.BadRequestException;
+import com.b2international.snowowl.eventbus.IEventBus;
+
 @RestController
 public class IndexController {
+	
+	private static final String REPOSITORY_ID = AtcCoreActivator.REPOSITORY_UUID;
+	private IEventBus bus;
+	
+	
+	
+	
+	public IndexController() {
+		bus = ApplicationContext.getInstance().getService(IEventBus.class);
+
+	}
+
+
 	@GetMapping("/")
-	public String index() {
-		return "someValue";
+	public AtcConcepts index() {
+		try {
+			final AtcConcepts concepts = AtcRequests
+					.prepareSearchConcept()
+					.all()
+					.build(REPOSITORY_ID, IBranchPath.MAIN_BRANCH)
+					.execute(bus)
+					.getSync();
+			
+			return concepts;
+			
+		} catch(Exception e) {
+			throw new BadRequestException(e.getMessage());
+		}
+		
+		
+	
 	}
 }
