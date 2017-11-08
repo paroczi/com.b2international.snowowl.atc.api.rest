ecf232fefa824aa4ef3b3d643d5016b4c7211427
Paróczi Gergő <gergo.paroczi@gmail.com>
2017-11-08 12:40:36 +0100

    get concept by id

--- src/com/b2international/snowowl/atc/api/rest/IndexController.java
+++ src/com/b2international/snowowl/atc/api/rest/IndexController.java
@@ -1,9 +1,11 @@
 package com.b2international.snowowl.atc.api.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.b2international.snowowl.atc.core.AtcCoreActivator;
+import com.b2international.snowowl.atc.core.domain.AtcConcept;
 import com.b2international.snowowl.atc.core.domain.AtcConcepts;
 import com.b2international.snowowl.atc.core.request.AtcRequests;
 import com.b2international.snowowl.core.ApplicationContext;
@@ -13,36 +15,41 @@ import com.b2international.snowowl.eventbus.IEventBus;
 
 @RestController
 public class IndexController {
-	
+
 	private static final String REPOSITORY_ID = AtcCoreActivator.REPOSITORY_UUID;
 	private IEventBus bus;
-	
-	
-	
-	
+
 	public IndexController() {
 		bus = ApplicationContext.getInstance().getService(IEventBus.class);
-
 	}
 
-
-	@GetMapping("/")
-	public AtcConcepts index() {
+	@GetMapping("/concepts")
+	public AtcConcepts getAllConcepts() {
 		try {
-			final AtcConcepts concepts = AtcRequests
-					.prepareSearchConcept()
+			final AtcConcepts concepts = AtcRequests.prepareSearchConcept()
 					.all()
 					.build(REPOSITORY_ID, IBranchPath.MAIN_BRANCH)
 					.execute(bus)
 					.getSync();
-			
 			return concepts;
-			
-		} catch(Exception e) {
+		} catch (Exception e) {
 			throw new BadRequestException(e.getMessage());
 		}
-		
-		
-	
 	}
+	
+	@GetMapping("/concepts/{conceptId}")
+	public AtcConcept getConteptById(
+			@PathVariable(value="conceptId")
+			final String conceptId
+	) {
+		try {
+			return AtcRequests.prepareGetConcept(conceptId)
+					.build(REPOSITORY_ID, IBranchPath.MAIN_BRANCH)
+					.execute(bus)
+					.getSync();
+		} catch (Exception e) {
+			throw new BadRequestException(e.getMessage());
+		}
+	}
+	
 }
