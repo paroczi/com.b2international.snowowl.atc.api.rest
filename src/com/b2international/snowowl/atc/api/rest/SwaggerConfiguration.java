02f2fcb0f2a49f390ee7eae461611e0ed03da94f
Paróczi Gergő <gergo.paroczi@gmail.com>
2017-11-27 21:59:18 +0100

    add fix

--- src/com/b2international/snowowl/atc/api/rest/SwaggerConfiguration.java
+++ src/com/b2international/snowowl/atc/api/rest/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package com.b2international.snowowl.atc.api.rest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.google.common.base.Predicates;
 
@@ -11,21 +12,22 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.spi.DocumentationType;
 
 @Configuration 
+@EnableWebMvc
 @EnableSwagger2
 public class SwaggerConfiguration {
-
-	 @Bean
-	  public Docket swaggerSpringMvcPlugin() {
-	    return new Docket(DocumentationType.SWAGGER_2)
-	            .groupName("ATC")
-	            .select() 
-	            .apis(Predicates.alwaysTrue())
-	            .paths(Predicates.alwaysTrue()) // and by paths
-	            .build();
-//	            .apiInfo(apiInfo())
-//	            .securitySchemes(securitySchemes())
-//	            .securityContext(securityContext());
-	  }
+//
+//	 @Bean
+//	  public Docket swaggerSpringMvcPlugin() {
+//	    return new Docket(DocumentationType.SWAGGER_2)
+//	            .groupName("ATC")
+//	            .select() 
+//	            .apis(Predicates.alwaysTrue())
+//	            .paths(Predicates.alwaysTrue()) // and by paths
+//	            .build();
+////	            .apiInfo(apiInfo())
+////	            .securitySchemes(securitySchemes())
+////	            .securityContext(securityContext());
+//	  }
 
 
 }
